e74a423559dd92520a4e37ed8f8e61be67015212
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2022-05-07 00:00:01 +0200

    Trim trailing whitespace

--- qol/add_extract_command.sh
+++ qol/add_extract_command.sh
@@ -8,6 +8,6 @@
 clear
 echo "Proceeding to add 'extract' command to easier extract a lot of archive types. Press any key to continue."
 read -rsn1
-sudo tee /etc/profile.d/extract.sh 
+sudo tee /etc/profile.d/extract.sh
 sudo wget https://raw.githubusercontent.com/xvoland/Extract/a501062ea37e53d23eff1c911dfc1c446bd381a1/extract.sh -O /etc/profile.d/extract.sh
 sudo chmod +x /etc/profile.d/extract.sh
