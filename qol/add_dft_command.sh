3f2c49b9213b874594d3f3e9366b818b8286a9d9
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2022-05-06 17:56:42 +0200

    Add dfty command

--- qol/add_dft_command.sh
+++ qol/add_dft_command.sh
@@ -6,3 +6,4 @@ clear
 echo "Proceeding to add 'dft' command to update all dnf and flatpak packages then shutdown the system. Press any key to continue."
 echo "# DoneForToday command" | sudo tee -a /etc/bashrc
 echo "alias dft='sudo dnf upgrade && flatpak upgrade && shutdown 0'" | sudo tee -a /etc/bashrc
+echo "alias dfty='sudo dnf upgrade -y && flatpak upgrade -y && shutdown 0'" | sudo tee -a /etc/bashrc
