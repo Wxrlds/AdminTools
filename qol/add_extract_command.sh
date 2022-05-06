32bd9c066d51750c3e086b801425435fceec22df
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2022-05-06 23:55:12 +0200

    Add extract command

---
+++ qol/add_extract_command.sh
@@ -0,0 +1,13 @@
+##################################################
+## https://github.com/Wxrlds/FedoraSetupScripts ##
+###########################################################################################################
+## Based on:                                                                                             ##
+## https://raw.githubusercontent.com/xvoland/Extract/a501062ea37e53d23eff1c911dfc1c446bd381a1/extract.sh ##
+###########################################################################################################
+
+clear
+echo "Proceeding to add 'extract' command to easier extract a lot of archive types. Press any key to continue."
+read -rsn1
+sudo tee /etc/profile.d/extract.sh 
+sudo wget https://raw.githubusercontent.com/xvoland/Extract/a501062ea37e53d23eff1c911dfc1c446bd381a1/extract.sh -O /etc/profile.d/extract.sh
+sudo chmod +x /etc/profile.d/extract.sh
