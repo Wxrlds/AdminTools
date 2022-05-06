d8c6a8d08948fbff04acdf034271ba469de9e7a1
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2022-05-06 17:56:05 +0200

    Fix dft alias

--- qol/add_dft_command.sh
+++ qol/add_dft_command.sh
@@ -5,4 +5,4 @@
 clear
 echo "Proceeding to add 'dft' command to update all dnf and flatpak packages then shutdown the system. Press any key to continue."
 echo "# DoneForToday command" | sudo tee -a /etc/bashrc
-echo dft='sudo dnf upgrade && flatpak upgrade && shutdown 0' | sudo tee -a /etc/bashrc
+echo "alias dft='sudo dnf upgrade && flatpak upgrade && shutdown 0'" | sudo tee -a /etc/bashrc
