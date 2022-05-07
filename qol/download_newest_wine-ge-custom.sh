f0d3b972ab1f6d79869ee1d251f7279f4829b959
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2022-05-07 11:56:22 +0200

    Add download and move custom wine runner
    by GloriousEggroll https://github.com/GloriousEggroll/wine-ge-custom/releases

---
+++ qol/download_newest_wine-ge-custom.sh
@@ -0,0 +1,15 @@
+##################################################
+## https://github.com/Wxrlds/FedoraSetupScripts ##
+################################################################
+## https://github.com/GloriousEggroll/wine-ge-custom/releases ##
+################################################################
+echo "Proceeding to download newest Wine-GE-Custom by GloriousEggroll and moving it to the Lutris directory. Press any key to continue."
+read -rsn1
+echo "Getting Wine-GE-Custom download URL"
+winegecustomurl=`curl -s https://api.github.com/repos/GloriousEggroll/wine-ge-custom/releases/latest | grep "browser_download_url.*tar.xz" | cut -d : -f 2,3 | tr -d \" `
+echo "Downloading Wine-GE-Custom"
+wget $winegecustomurl -c -P ~/Downloads --show-progress
+echo "Extracting Wine-GE-Custom"
+tar -xvf wine-lutris-GE*.tar.xz
+rm wine-lutris-GE*.tar.xz
+mv lutris-GE-* ~/.local/share/lutris/runners/wine
