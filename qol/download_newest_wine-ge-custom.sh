2590859eb3e9d88b21f86f00ccdf0672b41c9754
Wxrlds <42120270+Wxrlds@users.noreply.github.com>
2022-09-10 20:53:15 +0200

    Make download wine ge custom an alias

--- qol/download_newest_wine-ge-custom.sh
+++ qol/download_newest_wine-ge-custom.sh
@@ -4,10 +4,4 @@
 ## https://github.com/GloriousEggroll/wine-ge-custom/releases ##
 ################################################################
 # Download Wine-GE-Custom by GloriousEggroll and move it to Lutris directory
-echo "Getting Wine-GE-Custom download URL"
-winegecustomurl=`curl -s https://api.github.com/repos/GloriousEggroll/wine-ge-custom/releases/latest | grep "browser_download_url.*tar.xz" | cut -d : -f 2,3 | tr -d \" `
-echo "Downloading Wine-GE-Custom"
-wget $winegecustomurl -c -P ~/Downloads --show-progress
-echo "Extracting Wine-GE-Custom"
-tar -xvf ~/Downloads/wine-lutris-GE*.tar.xz --directory ~/.local/share/lutris/runners/wine/
-rm ~/Downloads/wine-lutris-GE*.tar.xz
+alias download-wine-ge-custom='winegecustomurl=`curl -s https://api.github.com/repos/GloriousEggroll/wine-ge-custom/releases/latest | grep "browser_download_url.*tar.xz" | cut -d : -f 2,3 | tr -d \" ` && wget $winegecustomurl -c -P ~/Downloads --show-progress && tar -xvf ~/Downloads/wine-lutris-GE*.tar.xz --directory ~/.local/share/lutris/runners/wine/ && rm ~/Downloads/wine-lutris-GE*.tar.xz'
